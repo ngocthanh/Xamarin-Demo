9d78800349b1f9424f9c5a82be6392073dab2511
LeThanh <lethanh@LeThanhs-Mac.local>
2016-03-28 07:52:58 +0700

    add project

---
+++ Demo/Demo/Class/UI/Login.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Demo
+{
+	public partial class Login : ContentPage
+	{
+		public Login ()
+		{
+			InitializeComponent ();
+			btn1.Clicked += OnButtonClicked;
+		}
+
+		void OnButtonClicked(object sender, EventArgs e)
+		{
+			
+		}
+	}
+}
+
